ff6f4348c7f1499e334d87950c3636d3e352c48f
yohannesAlemayehu <152785043+yohannesAlemayehu@users.noreply.github.com>
2024-03-08 15:13:53 +0300

    Create LAB-1-Functions-1.dart

---
+++ LAB-1-Functions-1.dart
@@ -0,0 +1,9 @@
+void main() {
+  double number1 = 5.7;
+  double number2 = 3.2;
+  double sum = addNumbers(number1, number2);
+  print("The sum of $number1 and $number2 is: $sum");
+}
+double addNumbers(double a, double b) {
+  return a + b;
+}
