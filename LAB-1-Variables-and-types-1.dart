44a8d1f18271ac2b362a587279862502d101c1ff
yohannesAlemayehu <152785043+yohannesAlemayehu@users.noreply.github.com>
2024-03-08 14:40:01 +0300

    Create LAB-1-Variables-and-types-1.dart
    LAB-1-Variables-and-types-1

---
+++ LAB-1-Variables-and-types-1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Yohannes";
+  int age = 21; 
+  String favoriteColor = "Black";
+  print("Name: $name");
+  print("Age: $age");
+  print("Favorite Color: $favoriteColor");
+}
